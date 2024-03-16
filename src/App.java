097e5a5dcc8573a8f31a738e1db1de6228c2dbb5
Gustavo Soares Rodrigues - 823133821 <823133821@ulife.com.br>
2024-03-15 21:02:15 -0300

    exemplos

---
+++ src/App.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] vetor = new int[10];
+
+        for (int i = 0; i < vetor.length; i++) {
+            int number = random.nextInt(101);
+            vetor[i] = number;
+        }
+
+        ordenaQuickSortInt(vetor, 0, 9);
+
+        imprimirVetor(vetor);
+    }
+
+    public static void ordenaQuickSortInt(int vetor[], int baixo, int alto) {
+        int P, B, A;
+        int Pivo;
+
+        B = baixo;
+        A = alto;
+        Pivo = vetor[baixo];
+
+        while (baixo < alto) {
+            while ((vetor[alto] >= Pivo) && (baixo < alto)) {
+                alto--;
+            }
+            if (baixo != alto) {
+                vetor[baixo] = vetor[alto];
+                baixo++;
+            }
+            while ((vetor[baixo] <= Pivo) && (baixo < alto)) {
+                baixo++;
+            }
+            if (baixo != alto) {
+                vetor[alto] = vetor[baixo];
+                alto--;
+            }
+        }
+
+        vetor[baixo] = Pivo;
+        P = baixo;
+        baixo = B;
+        alto = A;
+
+        if (baixo < P) ordenaQuickSortInt(vetor, baixo, P - 1);
+        if (alto > P) ordenaQuickSortInt(vetor, P + 1, alto);
+    }
+
+    public static void imprimirVetor(int vetor[]) {
+        for (int element : vetor) {
+            System.out.print(element + " ");
+        }
+    }
+}
